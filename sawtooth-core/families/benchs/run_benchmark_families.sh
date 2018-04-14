910d1461e46da1e4bcf4f470f4cd2e34a84dcfcc
TonyPhung <tonybka@TonyPhung.local>
2018-04-15 05:59:35 +0700

    SAW-1 families done

--- sawtooth-core/families/benchs/run_benchmark_families.sh
+++ sawtooth-core/families/benchs/run_benchmark_families.sh
@@ -5,10 +5,8 @@ wait
 benchs --benchmark-autosave --benchmark-enable --benchmark-verbose --benchmark-json=/project/result/families_blockinfo_sawtoothblockinfo_processor.json ./bench_block_info/families_blockinfo_sawtoothblockinfo_processor.py
 wait
 benchs --benchmark-autosave --benchmark-enable --benchmark-verbose --benchmark-json=/project/result/families_identity_sawtoothidentity_processor.json ./bench_identity/families_identity_sawtoothidentity_processor.py
-
-
-
-
+wait
+benchs --benchmark-autosave --benchmark-enable --benchmark-verbose --benchmark-json=/project/result/families_settings_processor.json ./bench_settings/families_settings_processor.py
 find . -type d -name __pycache__ -exec rm -r {} \+
 rm -Rf ./.benchmarks
 find . -type d -name .cache -exec rm -r {} \+
